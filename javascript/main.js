bb70c060a4db2a09067ed8f05f668274e89c54ef
vishal <vishalninaniya8949info@gmail.com>
2024-03-22 18:15:37 +0530

    create navbar

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,16 @@
+const linkList = document.querySelectorAll(".link-list");
+const icons = document.querySelectorAll(".icons");
 
+for (let i = 0; i < linkList.length; i++) {
+  linkList[i].addEventListener("mouseover", () => {
+    linkList[i].classList.add("active");
+    linkList[i].style.width = "90%";
+    icons[i].style.color = "#fff";
+  });
+
+  linkList[i].addEventListener("mouseout", () => {
+    linkList[i].classList.remove("active");
+    linkList[i].style.width = "40%";
+    icons[i].style.color = "#000";
+  });
+}
