997ee814ec1a89428f348e7b42f2fe061b33bf13
vishal <vishalninaniya8949info@gmail.com>
2024-08-18 14:57:02 +0530

    add themes

--- javascript/main.js
+++ javascript/main.js
@@ -1,31 +1,41 @@
 import projects from "./projects.json";
 import { card } from "./card";
 import { progressBar } from "./progress-bar";
+import { themeToggler } from "./web-theme"
 
-// observering the skill-section when it's in viewport to load skills animation
-const skillSection = document.querySelector(".skills");
-const skillSectionObserver = new IntersectionObserver(
-  (entry, observer) => {
-    const [ent] = entry;
 
-    if (!ent.isIntersecting) return;
-    progressBar();
 
-    observer.unobserve(skillSection);
-  },
-  {
-    root: null,
-    threshold: 0,
-  }
-);
 
-skillSectionObserver.observe(skillSection);
+function init() {
+  themeToggler();
+  // observering the skill-section when it's in viewport to load skills animation
+  const skillSection = document.querySelector(".skills");
+  const skillSectionObserver = new IntersectionObserver(
+    (entry, observer) => {
+      const [ent] = entry;
 
-card(projects);
+      if (!ent.isIntersecting) return;
+      progressBar();
 
-const header = document.querySelector(".header-section");
-const humburgerMenu = document.querySelector(".humburger-menu");
+      observer.unobserve(skillSection);
+    },
+    {
+      root: null,
+      threshold: 0,
+    }
+  );
 
-humburgerMenu.addEventListener("click", () => {
-  header.classList.toggle("nav-active");
-});
+  skillSectionObserver.observe(skillSection);
+
+  card(projects);
+
+  const header = document.querySelector(".header-section");
+  const humburgerMenu = document.querySelector(".humburger-menu");
+
+  humburgerMenu.addEventListener("click", () => {
+    header.classList.toggle("nav-active");
+  });
+};
+
+init();
+
---
+++ javascript/web-theme.js
@@ -0,0 +1,7 @@
+const sunButton = document.querySelector(".sun-icon");
+const moonButton = document.querySelector(".moon-icon");
+
+const currentTheme = localStorage.getItem('web-theme') || 'light';
+export function themeToggler() {
+
+};
