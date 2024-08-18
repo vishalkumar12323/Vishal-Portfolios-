7436ee49d18e63a48b16fa170f9a93e6b950d7e4
vishal <vishalninaniya8949info@gmail.com>
2024-08-18 22:17:48 +0530

    adding top navigation button

--- javascript/main.js
+++ javascript/main.js
@@ -4,10 +4,16 @@ import { progressBar } from "./progress-bar";
 import { themeToggler } from "./web-theme"
 
 
-
-
 function init() {
+  const mainSection = document.querySelector("#main");
+
+  const buttonElement = document.createElement('button');
+  buttonElement.classList.add("top-button");
+  buttonElement.innerHTML = `<ion-icon name="chevron-up-outline"></ion-icon>`
+  document.body.appendChild(buttonElement);
+
   themeToggler();
+
   // observering the skill-section when it's in viewport to load skills animation
   const skillSection = document.querySelector(".skills");
   const skillSectionObserver = new IntersectionObserver(
@@ -15,7 +21,7 @@ function init() {
       const [ent] = entry;
 
       if (!ent.isIntersecting) return;
-      // progressBar();
+      progressBar();
 
       observer.unobserve(skillSection);
     },
@@ -29,12 +35,35 @@ function init() {
 
   card(projects);
 
+  const mainSectionObserver = new IntersectionObserver((entries) => {
+    const [ent] = entries;
+
+    if (!ent.isIntersecting) {
+      buttonElement.style.opacity = "0";
+    } else {
+      buttonElement.style.opacity = "1";
+      buttonElement.addEventListener("click", () => {
+        window.scrollTo({ behavior: 'smooth', top: 0 });
+      });
+    }
+  }, { root: null, threshold: 0 });
+
+  mainSectionObserver.observe(mainSection);
+
   const header = document.querySelector(".header-section");
   const humburgerMenu = document.querySelector(".humburger-menu");
+  const navbar = document.querySelector("#navbar");
 
   humburgerMenu.addEventListener("click", () => {
     header.classList.toggle("nav-active");
   });
+
+  header.addEventListener('click', (e) => {
+    e.stopPropagation();
+    header.classList.remove("nav-active");
+  });
+
+  navbar.addEventListener('click', (e) => e.stopPropagation());
 };
 
 init();
--- javascript/web-theme.js
+++ javascript/web-theme.js
@@ -6,7 +6,7 @@ const sectionTitle = document.querySelectorAll(".section-title h2");
 export function themeToggler() {
     let theme = localStorage.getItem('web-theme') || 'dark';
     document.body.classList.add(theme);
-    button.innerHTML = theme === "dark" ? `<ion-icon name="moon-outline" size="large"></ion-icon>` : `<ion-icon name="sunny-outline" size="large"></ion-icon>`;
+    button.innerHTML = theme === "dark" ? `<ion-icon name="moon-outline"></ion-icon>` : `<ion-icon name="sunny-outline"></ion-icon>`;
 
     heroSection.style.backgroundColor = theme === 'dark' ? '#020202' : '#f5f6fa';
     footerSection.style.backgroundColor = theme === 'dark' ? '#020202' : '#f5f6fa';
@@ -15,7 +15,7 @@ export function themeToggler() {
         if (theme === "dark") {
             document.body.classList.remove('dark');
             document.body.classList.add('light');
-            button.innerHTML = `<ion-icon name="sunny-outline" size="large"></ion-icon>`
+            button.innerHTML = `<ion-icon name="sunny-outline"></ion-icon>`
             localStorage.setItem("web-theme", 'light');
             heroSection.style.backgroundColor = '#f5f6fa'
             footerSection.style.backgroundColor = '#f5f6fa'
@@ -24,7 +24,7 @@ export function themeToggler() {
         } else {
             document.body.classList.remove('light');
             document.body.classList.add('dark');
-            button.innerHTML = `<ion-icon name="moon-outline" size="large"></ion-icon>`
+            button.innerHTML = `<ion-icon name="moon-outline"></ion-icon>`
             heroSection.style.backgroundColor = '#020202'
             footerSection.style.backgroundColor = '#020202'
             sectionTitle.forEach((ele) => ele.style.color = '#fff');
